d5e7a7786884c6a48165cd4a8e94dc99ed062ecf
Miguel A. Nuñez Aparicio <miguel.angel.nap@gmail.com>
2016-04-12 11:23:14 +0200

    Splash implementado, icono y nombre de aplicacion tambien implementados.

---
+++ app/src/main/java/com/example/miguel/openhousemadrid/Splash.java
@@ -0,0 +1,30 @@
+package com.example.miguel.openhousemadrid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        openApp(true);
+    }
+
+    private void openApp(boolean locationPermission) {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash
+                        .this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+
+}
